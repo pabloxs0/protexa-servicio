44e7022b8a263f4d7c3e2d278a6ba6a89e10329a
pabloxs0 <pabloxs@msn.com>
2024-08-19 17:01:58 -0600

    Commit 1

--- src/index.js
+++ src/index.js
@@ -19,9 +19,10 @@ app.use(bodyParser.json())
 // })
 
 //routes
-app.use(require('./routes/cobranza'));
-app.use(require('./routes/cambio_estado'));
-app.use(require('./routes_stp_ws/stp_ws'));
+app.use(require('./routes/prueba'));
+// app.use(require('./routes/cobranza'));
+// app.use(require('./routes/cambio_estado'));
+// app.use(require('./routes_stp_ws/stp_ws'));
 //app.use(require('./routes/pruebas'));
 //start server
 app.listen(app.get('port'),()=> {
