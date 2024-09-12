913beb9f416be0f5f4f4216a30b8b14cc76a93d8
pabloxs0 <pabloxs@msn.com>
2024-09-12 11:57:14 -0600

    Commit 1

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 
 //routes
 app.use(require('./routes/prueba'));
-// app.use(require('./routes/cobranza'));
+app.use(require('./routes/ws_test'));
 // app.use(require('./routes/cambio_estado'));
 // app.use(require('./routes_stp_ws/stp_ws'));
 //app.use(require('./routes/pruebas'));
