3df380bbb4f1dbddac38864657cef700d62ab795
pabloxs0 <pabloxs@msn.com>
2024-09-12 21:10:23 -0600

    Commit 1

--- src/ws_protexa/ws_protexa.js
+++ src/ws_protexa/ws_protexa.js
@@ -36,7 +36,7 @@ async function consume_soap(req, res) {
                 return;
             }
 
-            res.send("ERROR AL HACER EL REQUEST!!!" + JSON.stringify(client));
+
 
             // Make a SOAP request
             //const args = { EWerks: '6110', TMatnr:[{Matnr:'T4214160'},{Matnr:'T100000004'}], };
@@ -56,7 +56,7 @@ async function consume_soap(req, res) {
                     //     }
                     // }
                    // res.send("ERROR AL HACER EL REQUEST!!!" + toStr);
-                   res.send("ERROR AL HACER EL REQUEST!!!" + JSON.stringify(err));
+                   res.send("ERROR AL HACER EL REQUEST!!!" + err.message);
                     return;
                 }
 
