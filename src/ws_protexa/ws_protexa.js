ba835f897c92e768b99e39e7029498e4e726dcf0
pabloxs0 <pabloxs@msn.com>
2024-09-12 19:44:24 -0600

    Commit 1

--- src/ws_protexa/ws_protexa.js
+++ src/ws_protexa/ws_protexa.js
@@ -42,7 +42,7 @@ async function consume_soap(req, res) {
             client.ZfmMmConsultInvt(args, function(err, result) {
                 if (err) {
                     console.log('Error making SOAP request:', err);
-                    res.send("ERROR AL HACER EL REQUEST!!!" + err.body);
+                    res.send("ERROR AL HACER EL REQUEST!!!" + err);
                     return;
 
                     //return;
