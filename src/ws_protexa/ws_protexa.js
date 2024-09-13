6f34e97e3b17f85e118b0995bfeeedf44ab1241d
pabloxs0 <pabloxs@msn.com>
2024-09-12 21:03:04 -0600

    Commit 1

--- src/ws_protexa/ws_protexa.js
+++ src/ws_protexa/ws_protexa.js
@@ -49,7 +49,7 @@ async function consume_soap(req, res) {
                 // rawRequest is the raw xml request string
                 if (err) {
                     console.log('CCCCCC:', err);
-                    res.send("ERROR AL HACER EL REQUEST!!!" + soapHeader);
+                    res.send("ERROR AL HACER EL REQUEST!!!" + rawRequest);
                     return;
                 }
 
